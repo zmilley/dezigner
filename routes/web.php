4c6de2cfe4454311f72ff23041b44c0408f27d68
Kim Kristensen <kimkristensen@Kims-MacBook-Pro.local>
2017-01-31 21:21:18 +0100

    Elequent

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Task;
 
-Route::get('/', function () {
-    $tasks = [
-        'Fisk',
-        'Sovs',
-        'Persille'
-    ];
+Route::get('/tasks', function () {
+
+    $tasks = Task::all();
+
     
-    return view('welcome', compact('tasks'));
+    
+
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{id}', function ($id) {
+
+    $task = Task::find($id);
+
+    
+
+    return view('tasks.show', compact('task'));
 });
